8947fdaed107f98d50716e9bea0e6fcd8c5871c3
mdg081 <mdg081@gmail.com>
2026-03-07 15:32:00 +0900

    제목변경

---
+++ test.py
@@ -0,0 +1,8 @@
+﻿import codecs
+with open('lockring-search.html', 'r', encoding='utf-8') as f:
+    lines = f.readlines()
+    for i, line in enumerate(lines):
+        if len(line) > 1000:
+            print(f'Line {i+1} is very long: {len(line)} characters')
+        else:
+            print(f'Line {i+1}: {line.strip()!r}')
